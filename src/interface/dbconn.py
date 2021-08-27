15d903fdbae4ad8302f3d6754d53b5552dd3d2d6
Samuel Tregea <sdtregea@gmail.com>
2021-08-26 20:43:22 -0400

    Add an interface to the project to allow for interaction with the database as well as helper files.

---
+++ src/interface/dbconn.py
@@ -0,0 +1,62 @@
+import sqlite3
+from sqlite3 import Error
+from utils.logger.logger import log
+
+
+class DB:
+    """
+    This class will serve as the interface between client and the database.
+    """
+
+    def __init__(self, db_file) -> None:
+        """
+        Set up a connection to the database.
+        :param db_file: The database to connect to.
+        """
+        self.conn = None
+        try:
+            self.conn = sqlite3.connect(db_file)
+        except Error as e:
+            log(str(e), level="error")
+
+    def close(self) -> None:
+        """
+        Close the connection to the database
+        """
+        self.conn.close()
+
+    def commit(self, query: str, values: tuple = None) -> None:
+        """
+        Commit a query to the database.
+        :param query: The  query to commit.
+        :param values: The values associated with the query.
+        """
+        cursor = self.conn.cursor()
+
+        if values is not None and len(values) > 0:
+            cursor.execute(query, values)
+        else:
+            cursor.execute(query)
+
+        cursor.close()
+        # log("Query committed.", level="debug")
+        self.conn.commit()
+
+    def fetchall(self, query: str, values: tuple = None) -> list:
+        """
+        Perform a fetchall from a selection.
+        :param query: The query to perform.
+        :param values: The values associated with the query.
+        :return: a list of tuples from the query.
+        """
+        cursor = self.conn.cursor()
+
+        if values is not None and len(values) > 0:
+            cursor.execute(query, values)
+        else:
+            cursor.execute(query)
+
+        # log("Query fetched.", level="debug")
+        data = cursor.fetchall()
+        cursor.close()
+        return data
---
+++ src/utils/globals.py
@@ -0,0 +1,7 @@
+import os
+
+# Database path
+DATABASE = os.path.abspath('../imdb.db')
+
+# The log file containing the pertinent information about the program
+LOG_FILE = os.path.abspath('../imdb.log')
