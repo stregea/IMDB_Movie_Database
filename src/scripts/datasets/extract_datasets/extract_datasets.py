9a260a7ea4a4526a8b48f0e1a57276818cd34833
Samuel Tregea <sdtregea@gmail.com>
2021-08-27 09:13:02 -0400

    Move get_list_of_files to a more accessible location within the project.

--- src/scripts/datasets/extract_datasets/extract_datasets.py
+++ src/scripts/datasets/extract_datasets/extract_datasets.py
@@ -3,22 +3,7 @@ import os
 import shutil
 from utils.globals import DATASETS
 from utils.logger.logger import log
-
-
-def get_list_of_files(directory: str, file_type: str) -> list:
-    """
-    Search a specified directory and add the file's specified by a file type to a list.
-    :param directory: The directory to search.
-    :param file_type: The type of file to add to the list.
-    :return: A list of files within the specified directory.
-    """
-    ret = []
-
-    for (root, subdirectories, files) in os.walk(directory):
-        for file in files:
-            if file.endswith(file_type):
-                ret.append(os.path.abspath(os.path.join(root, file)))
-    return ret
+from utils.walkers.directory_walker import get_list_of_files
 
 
 def extract_dataset_files() -> None:
