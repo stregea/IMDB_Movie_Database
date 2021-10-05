51bfd3ac64f8b69fd7d9e0d2457a3ba645f21b7b
nmm9478 <nikitamassaria@gmail.com>
2021-10-05 03:45:52 -0400

    Added basic outlines for needed functionality, in the files analysis.py and analysis_specific.py in analysis_functionality folder (in 'data_analysis' in 'scripts'), and visualization.py in data_visualization folder (in 'data_analysis' in 'scripts').

---
+++ src/scripts/data_analysis/analysis_functionality/analysis.py
@@ -0,0 +1,49 @@
+"""
+Data analysis basic functions that will be called by analysis_specific.
+"""
+
+
+
+
+def get_genre_information() -> None:
+    """
+    Genre analysis script.
+    Will be called by more specific functions.
+    """
+    return
+
+
+def get_run_time_mins_information():
+    """
+    Will be called by more specific functions.
+    """
+    return
+
+def get_region_information():
+    """
+    Will be called by more specific functions
+    """
+    return
+
+def primary_profession_total():
+    """
+    What % of the total data is each primary profession?
+    - Must not double count the same nconst as multiple people.
+      Number of unique nconsts is the total data to compare against.
+    """
+    return
+
+def primary_profession_amount():
+    """
+    What % of people (nconst) have one primary profession? 2? 3 or more?
+    Calculates this.
+    Visualization method to be determined.
+    """
+    return
+
+
+
+
+
+
+
