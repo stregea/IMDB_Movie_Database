a51804e32b07149fc1f266ceaac8e18b37226f0d
Samuel Tregea <sdtregea@gmail.com>
2021-10-12 10:26:31 -0400

    Define the histogram() functionality.

--- src/scripts/data_analysis/data_visualization/visualization.py
+++ src/scripts/data_analysis/data_visualization/visualization.py
@@ -2,45 +2,54 @@
 File for data visualization.
 Make different types of graphs.
 """
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
+# set the seaborn theme for the visualizations
+sns.set()
 
-# histogram
 
-def make_histogram():
+def histogram(df: pd.DataFrame, attribute_to_visualize: str, attributes: tuple[str, str]) -> None:
     """
-    Basic function that makes a histogram
+    Display a Histogram visualization
+    :param df: Pandas dataframe containing the information to visualize.
+    :param attribute_to_visualize: Semantic variable that is mapped to determine the color of plot elements.
+    :param attributes: Tuple that contains the variables that specify positions on the x and y axes.
     """
-    return
+    sns.histplot(data=df, hue=attribute_to_visualize, x=attributes[0], y=attributes[1], multiple="stack")
+    plt.show()
+
 
 def make_histogram_checker():
     """
     run unique values function for all non-numeric data attributes.
     If less than 15 unique values, then make histogram for each.
-    (Calls make_histogram() )
+    (Calls histogram() )
     """
-    return
+    pass
 
 
-
-# bar chart
-def make_bar_chart():
+def bar_chart(df: pd.DataFrame) -> None:
     """
-    Makes a bar chart
+    Display a Histogram visualization
+    :param df: Pandas dataframe containing the information to visualize.
     """
-    return
-
-# pie chart
-def make_pie_chart():
-    """
-    Makes a pie chart
-    """
-    return
+    pass
 
 
-# scatter plot
-def make_scatter_plot():
+def pie_chart(df: pd.DataFrame) -> None:
     """
-    Makes a scatter plot
+    Display a Histogram visualization
+    :param data_frame: Pandas dataframe containing the information to visualize.
     """
-    return
+    pass
+
+
+def scatter_plot(df: pd.DataFrame) -> None:
+    """
+    Display a Histogram visualization
+    :param df: Pandas dataframe containing the information to visualize.
+    """
+    pass
