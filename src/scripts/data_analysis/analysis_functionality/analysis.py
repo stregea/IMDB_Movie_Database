31f9c4db0b3c658d98d8668b7eb2df79da9f9e66
Samuel Tregea <sdtregea@gmail.com>
2021-10-20 11:18:08 -0400

    Populate primary_profession_total()

--- src/scripts/data_analysis/analysis_functionality/analysis.py
+++ src/scripts/data_analysis/analysis_functionality/analysis.py
@@ -4,8 +4,8 @@ Data analysis basic functions that will be called by analysis_specific.
 import os
 import csv
 import pandas as pd
-from utils.globals import COMBINED, genres, runtimeMinutes, region
-# from scripts.data_analysis.data_visualization.visualization import bar_chart, histogram
+from utils.globals import COMBINED, genres, runtimeMinutes, region, nconst, primaryProfession
+from utils.helpers import convert_number_to_percentage
 
 
 def get_attributes_column_from_data(column_index: int, column_name: str) -> pd.DataFrame:
@@ -13,6 +13,7 @@ def get_attributes_column_from_data(column_index: int, column_name: str) -> pd.D
     Get the given column as a 1d list in a pandas DataFrame
     :param column_index: the numerical index of the column we are looking for
     :param column_name: the name of the column we are looking for
+    :return: A pandas DataFrame of the column specified from the dataset.
     """
     final_output_file = os.path.abspath(os.path.join(COMBINED, "final.output.tsv"))
 
@@ -37,6 +38,7 @@ def get_genre_information() -> pd.DataFrame:
     """
     Genre analysis script.
     Will be called by more specific functions.
+    :return: A pandas DataFrame containing information for the 'genres' attribute.
     """
     return get_attributes_column_from_data(column_index=genres, column_name='genres')
 
@@ -44,6 +46,7 @@ def get_genre_information() -> pd.DataFrame:
 def get_run_time_mins_information() -> pd.DataFrame:
     """
     Will be called by more specific functions.
+    :return: A pandas DataFrame containing information for the 'runtimeMinutes' attribute.
     """
     return get_attributes_column_from_data(column_index=runtimeMinutes, column_name='runtimeMinutes')
 
@@ -51,17 +54,55 @@ def get_run_time_mins_information() -> pd.DataFrame:
 def get_region_information():
     """
     Will be called by more specific functions
+    :return: A pandas DataFrame containing information for the 'region' attribute.
     """
     return get_attributes_column_from_data(column_index=region, column_name='region')
 
 
-def primary_profession_total():
+def primary_profession_total() -> dict:
     """
     What % of the total data is each primary profession?
     - Must not double count the same nconst as multiple people.
       Number of unique nconsts is the total data to compare against.
+    :return: A dictionary containing information of the total percentage of each profession.
+             The form the dictionary will be in is: { profession : percentage of total data }.
     """
-    return
+
+    final_output_file = os.path.abspath(os.path.join(COMBINED, "final.output.tsv"))
+
+    # Dictionary to contain the return values. Will be in the form { profession : percentage of total data }.
+    dictionary_of_professions = dict()
+
+    # Set to be used to determine the total size of the nconst attributes.
+    nconst_set = set()
+
+    with open(final_output_file, mode='r', encoding='utf-8') as file:
+        output_file = csv.reader(file, delimiter='\t')
+
+        # Skip the header file
+        next(output_file)
+
+        # iterate through the file
+        for record in output_file:
+
+            nconst_value = record[nconst]
+            profession = record[primaryProfession]
+
+            # add the nconst into the set if it doesn't already exist.
+            if nconst_value not in nconst_set:
+                nconst_set.add(nconst_value)
+
+            # count the total number of occurrences
+            if profession not in dictionary_of_professions:
+                dictionary_of_professions[profession] = 1
+            else:
+                dictionary_of_professions[profession] += 1
+
+    # change the total number of occurrences to percentages.
+    for profession in dictionary_of_professions:
+        dictionary_of_professions[profession] = dictionary_of_professions[profession] / len(nconst_set)
+
+    return dictionary_of_professions
 
 
 def primary_profession_amount():
