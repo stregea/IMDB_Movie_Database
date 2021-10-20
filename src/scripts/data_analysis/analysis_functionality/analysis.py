4550f2f77d569ed1a968f5ef784fbcf4181b4b3c
Samuel Tregea <sdtregea@gmail.com>
2021-10-20 12:01:56 -0400

    Populate primary_profession_amount()

--- src/scripts/data_analysis/analysis_functionality/analysis.py
+++ src/scripts/data_analysis/analysis_functionality/analysis.py
@@ -5,7 +5,7 @@ import os
 import csv
 import pandas as pd
 from utils.globals import COMBINED, genres, runtimeMinutes, region, nconst, primaryProfession
-from utils.helpers import convert_number_to_percentage
+from utils.helpers import convert_number_to_percentage, print_dict
 
 
 def get_attributes_column_from_data(column_index: int, column_name: str) -> pd.DataFrame:
@@ -85,8 +85,13 @@ def primary_profession_total() -> dict:
         # iterate through the file
         for record in output_file:
 
+            # grab the nconst and primaryProfession values.
             nconst_value = record[nconst]
             profession = record[primaryProfession]
+
+            # skip this individual if they have a null nconst attribute.
+            if nconst_value == '\\N':
+                continue
 
             # add the nconst into the set if it doesn't already exist.
             if nconst_value not in nconst_set:
@@ -94,21 +99,77 @@ def primary_profession_total() -> dict:
 
             # count the total number of occurrences
             if profession not in dictionary_of_professions:
-                dictionary_of_professions[profession] = 1
-            else:
+                dictionary_of_professions[profession] = 0
+
+            # Increment the total count for the current profession if not null.
+            if profession != '\\N':
                 dictionary_of_professions[profession] += 1
 
     # change the total number of occurrences to percentages.
+    total_number_of_people = len(nconst_set)
     for profession in dictionary_of_professions:
-        dictionary_of_professions[profession] = dictionary_of_professions[profession] / len(nconst_set)
+        dictionary_of_professions[profession] = convert_number_to_percentage(
+            dictionary_of_professions[profession] / total_number_of_people
+        )
+
+    print_dict(dictionary_of_professions)
 
     return dictionary_of_professions
 
 
-def primary_profession_amount():
+def get_primary_professions_information() -> dict:
+    """
+    Read through the data set and create a dictionary of each individual and their total number of professions.
+    :return: A dictionary containing all of the individuals and their total counted professions.
+             The form the dictionary will be in is: { nconst : professionCount }.
+    """
+    final_output_file = os.path.abspath(os.path.join(COMBINED, "final.output.tsv"))
+
+    nconst_dictionary = dict()
+
+    with open(final_output_file, mode='r', encoding='utf-8') as file:
+        output_file = csv.reader(file, delimiter='\t')
+
+        # Skip the header file
+        next(output_file)
+
+        # iterate through the file
+        for record in output_file:
+
+            # grab the nconst and primaryProfession values.
+            nconst_value = record[nconst]
+            profession = record[primaryProfession]
+
+            # skip this individual if they have a null nconst attribute.
+            if nconst_value == '\\N':
+                continue
+
+            # add the nconst into the dictionary if it doesn't already exist and create a sub dictionary.
+            if nconst_value not in nconst_dictionary:
+                nconst_dictionary[nconst_value] = 0
+
+            # Increment the number professions for this actor if not a null value.
+            if profession != '\\N':
+                nconst_dictionary[nconst_value] += 1
+
+    return nconst_dictionary
+
+
+def primary_profession_amount(number_of_professions: int) -> float:
     """
     What % of people (nconst) have one primary profession? 2? 3 or more?
     Calculates this.
     Visualization method to be determined.
+    :param number_of_professions: The specified number of professions we are looking for a person to have.
+    :return: The percentage of individuals who have the specified number of professions.
     """
-    return
+    nconst_dictionary = get_primary_professions_information()
+    total_number_of_people = len(nconst_dictionary)
+    count = 0
+
+    for nconst_value in nconst_dictionary:
+        profession_count_for_current_individual = nconst_dictionary[nconst_value]
+        if profession_count_for_current_individual == number_of_professions:
+            count += 1
+
+    return convert_number_to_percentage(count / total_number_of_people)
